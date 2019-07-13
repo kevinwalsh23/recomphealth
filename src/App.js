e0551da6e77c2e5f7675c172837be531a0887ee7
kevinwalsh23 <kevinwalsh23@gmail.com>
2019-07-13 12:27:45 -0400

    updating the site

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={"recomplogo.png"} className="App-logo" alt="logo" />
-        <p>
-          Recomp Health...coming soon!
-        </p>
-        
-      </header>
-    </div>
-  );
-}
+const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
 
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleSubmit(values) {
+      console.log("values: " + values)
+
+      console.log("Current State is: " + JSON.stringify(values));
+      alert("Current State is: " + JSON.stringify(values));
+      this.props.postFeedback(values);
+      this.props.resetFeedbackForm();
+    }  
+    render() {
+      return (
+        <div className="App">
+          <nav class="navbar bg-light titlefont">
+            <span class="navbar-brand mb-0 h1 titlefont">Recomp Health</span>
+          </nav>
+          <div className="container">
+            <div className="row">
+              <div className="col-12 m-1">
+                <h3 className="homefont"><b>The platform built to reward you for your healthy habits.</b></h3>
+                <p className="subfont">Recomp Health allows users to each cash back every day by logging their healthy habits in our app.</p>
+                <div className="formstyle">
+                <form class="form-inline formstyle" onSubmit={(values) => this.handleSubmit(values)}>
+                  <label class="sr-only" for="inlineFormInputName2">Name</label>
+                  <input type="email" class="form-control mb-2 mr-sm-2" id="inlineFormInputName2" placeholder="Enter Email"/>              
+                  <button type="submit" class="btn btn-secondary rounded mb-2">Request Access</button>
+                </form>
+                </div>      
+              </div>
+              <div className="col-12 m-1 imagestyle" >
+                <img src={"fit.jpg"} className="App-logo imagestyle" alt="logo" />
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+  }
+};
 export default App;
