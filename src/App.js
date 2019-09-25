42096ea9c243e0d23ecb2d286e4cde83b8fac36e
kevinwalsh23 <kevinwalsh23@gmail.com>
2019-09-25 00:27:36 -0400

    site updates

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ class App extends Component {
     }  
     render() {
       return (
+        <div>
         <div className="App">
           <nav className="navbar bg-light titlefont">
             <span className="navbar-size-new mb-0 h1 titlefont">Recomp Health</span>
@@ -27,7 +28,7 @@ class App extends Component {
           <div className="container">
             <div className="row">
               <div className="col-md m-1">
-                <h1 className="homefont"><b>The platform built to reward you for your healthy habits.</b></h1>
+                <h1 className="homefont textmargin"><b>The platform built to reward you for your healthy habits.</b></h1>
                 <h5 className="subfont">Recomp Health allows users to earn cash back every day by logging their healthy habits in our app.</h5>
                 <div className="formstyle">
                 <form className="form-inline formstyle" action="https://formspree.io/kevinwalsh23@gmail.com" method="POST">
@@ -37,10 +38,12 @@ class App extends Component {
                 </div>      
               </div>
               <div className="col-md m-1 imagestyle" >
-                <img src={"app1.png"} className="App-logo imagestyle" alt="logo" />
+                <img src={"rhprof.png"} className="App-logo imagestyle" alt="logo" />
               </div>
             </div>
           </div>
+        </div>
+
         </div>
       );
   }
