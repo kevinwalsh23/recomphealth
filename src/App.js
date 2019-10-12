198f5235b2b83887fafe19c5cf5bb5c32039421f
kevinwalsh23 <kevinwalsh23@gmail.com>
2019-10-12 15:25:43 -0400

    mobile update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import ReactGA from 'react-ga';
 
-const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
+// const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
 
 class App extends Component {
   constructor(props){
